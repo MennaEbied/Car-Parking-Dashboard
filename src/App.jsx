31c38c942340cfed7291ad49812811bd076dccec
MennaEbied <mennahesham253@gmail.com>
2024-11-26 15:18:37 +0200

    Set up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Bookings from "./pages/bookings/Bookings";
+import Home from "./pages/home/Home";
+import Single from "./pages/single/Single";
+import UsersList from "./pages/usersList/UsersList";
 
 function App() {
-  
-
   return (
-    <>
-      <div>
-        Dashboard
-      </div>
-    </>
-  )
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="bookings" element={<Bookings/>}/>
+            <Route path="users">
+              <Route index element={<UsersList/>}/>
+              <Route path=":userId" element={<Single/>}/>
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
