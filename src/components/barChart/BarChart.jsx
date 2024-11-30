27a398bbaeb031f1239414391927639cbd930a56
MennaEbied <mennahesham253@gmail.com>
2024-11-30 16:44:10 +0200

    customize barchart component

---
+++ src/components/barChart/BarChart.jsx
@@ -0,0 +1,79 @@
+import './barChart.scss'
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Rectangle,
+  ResponsiveContainer,
+} from "recharts";
+
+const data = [
+  {
+    name: "Sat",
+    duration:4
+   
+  },
+  {
+    name: "Sun",
+    duration:3
+   
+  },
+  {
+    name: "Mon",
+    duration:4
+  
+  },
+  {
+    name: "Tue",
+    duration:1
+   
+  },
+  {
+    name: "Wed",
+    duration:2
+   
+  },
+  {
+    name: "Thu",
+    duration:5
+ 
+  },
+  {
+    name: "Fri",
+    duration:8
+   
+  },
+];
+
+const Barchart=()=> {
+  return (
+    <div className="barChart">
+      <div className="title">Parking Duration</div>
+    <ResponsiveContainer width={350} height={300}>
+      <BarChart
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Bar
+          dataKey="duration"
+          fill="#4b49ac"
+          activeBar={<Rectangle  stroke="purple" />}
+        />
+      </BarChart>
+    </ResponsiveContainer>
+    </div>
+  );
+}
+export default Barchart;
