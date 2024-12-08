88382609746abba04757fb08661f467d86ea1c4d
Ebraheem <ebraheemsaid7@gmail.com>
2024-12-09 01:00:01 +0200

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,20 @@ import Reports from "./pages/reports/Reports";
 import Single from "./pages/single/Single";
 import Users from "./pages/users/Users";
 import UsersList from "./pages/usersList/UsersList";
-import './styles/dark.scss'
 
 function App() {
   return (
-    <div >
+    <div>
+      <div>HELLOOOOOOOOOOOO from ebraheem</div>
       <BrowserRouter>
         <Routes>
           <Route path="/">
             <Route index element={<Home />} />
-            <Route path="bookings" element={<Bookings/>}/>
-            <Route path="reports" element={<Reports/>}/>
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="reports" element={<Reports />} />
             <Route path="users">
-              <Route index element={<Users/>}/>
-              <Route path=":userId" element={<Single/>}/>
+              <Route index element={<Users />} />
+              <Route path=":userId" element={<Single />} />
             </Route>
           </Route>
         </Routes>
