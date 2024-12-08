b7ff94efd504433f78c89f72abf8a884390689b7
MennaEbied <mennahesham253@gmail.com>
2024-12-08 21:36:41 +0200

    Adjusting routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Bookings from "./pages/bookings/Bookings";
 import Home from "./pages/home/Home";
+import Reports from "./pages/reports/Reports";
 import Single from "./pages/single/Single";
+import Users from "./pages/users/Users";
 import UsersList from "./pages/usersList/UsersList";
+import './styles/dark.scss'
 
 function App() {
   return (
-    <div>
+    <div >
       <BrowserRouter>
         <Routes>
           <Route path="/">
             <Route index element={<Home />} />
             <Route path="bookings" element={<Bookings/>}/>
+            <Route path="reports" element={<Reports/>}/>
             <Route path="users">
-              <Route index element={<UsersList/>}/>
+              <Route index element={<Users/>}/>
               <Route path=":userId" element={<Single/>}/>
             </Route>
           </Route>
